6bdd4b39ba4bc5db8605662d045e29b7699240ad
Mohammad Maviya Khan <maviyamkhan16@gmail.com>
2026-04-03 13:52:42 +0530

    Add Bank interface and implementations for SBI, HDFC, ICICI

---
+++ Java Assignment/Bank.java
@@ -0,0 +1,33 @@
+interface Bank {
+    float getInterestRate();
+}
+
+class SBI implements Bank {
+    public float getInterestRate() {
+        return 7.0f;
+    }
+}
+
+class HDFC implements Bank {
+    public float getInterestRate() {
+        return 7.5f;
+    }
+}
+
+class ICICI implements Bank {
+    public float getInterestRate() {
+        return 8.0f;
+    }
+}
+
+class TestBank {
+    public static void main(String args[]) {
+        Bank b1 = new SBI();
+        Bank b2 = new HDFC();
+        Bank b3 = new ICICI();
+
+        System.out.println(b1.getInterestRate());
+        System.out.println(b2.getInterestRate());
+        System.out.println(b3.getInterestRate());
+    }
+}
