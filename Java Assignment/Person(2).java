9879954d4e37add360f35adfece84409049c33db
Mohammad Maviya Khan <maviyamkhan16@gmail.com>
2026-04-04 18:20:08 +0530

    Add Person, Employee, and Manager classes

---
+++ Java Assignment/Person(2).java
@@ -0,0 +1,22 @@
+class Person {
+    void role() {
+        System.out.println("I am a person");
+    }
+}
+
+class Employee extends Person {
+    void role() {
+        System.out.println("I am an employee");
+    }
+}
+
+class Manager extends Employee {
+    void role() {
+        System.out.println("I am a manager");
+    }
+
+    public static void main(String args[]) {
+        Person p = new Manager();
+        p.role();
+    }
+}
